8b7ed319d539d1a925a0362821a134868f382ae0
Kojo Bailey <kojomolojo@gmail.com>
2024-11-28 13:27:24 +0300

    BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
